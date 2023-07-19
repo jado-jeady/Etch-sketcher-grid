1b4a03da000337a75e5d3d694dbe5d3faa1bad15
jado-jeady <jeadynzaramyimana@gmail.com>
2023-07-19 16:43:04 +0200

    adding js Scripts

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Etch-sketcher-grid
+This is a sketching app ecactly like etch graph do.
+it is made by Html&Css and js
+As the Odin-project Guidance within andela kick-start program
+
+Click here for a [live previe](https://)
---
+++ app.js
@@ -0,0 +1,75 @@
+/* Functions */
+
+// Function for creating the hover effect
+let hoverEffect = () => {
+  $('main > div').hover(
+    (event) => {
+      $(`#${event.target.id}`).css(
+        'background-color',
+        `#${
+          rainbowColors[Math.floor(Math.random() * rainbowColors.length)].hex
+        }`
+      );
+    },
+    (event) => {
+      $(`#${event.target.id}`).css(
+        'background-color',
+        `#${
+          rainbowColors[Math.floor(Math.random() * rainbowColors.length)].hex
+        }`
+      );
+    }
+  );
+};
+
+// Function for creating the square grid
+let createGrid = (input) => {
+  $('main').css('grid-template-columns', `repeat(${input}, 1fr)`);
+  $('main').css('grid-template-rows', `repeat(${input}, 1fr)`);
+
+  for (let i = 0; i < Math.round(input ** 2); i++) {
+    $('main').append(`<div id="square${i + 1}" class="square"></div>`);
+  }
+
+  hoverEffect();
+};
+
+// Function for creating the prompt
+let input = () => {
+  return Number(
+    prompt(
+      'Enter number of squares on one side, length should be between 1 & 100'
+    )
+  );
+};
+
+/* --------- */
+
+// Rainbow Colors hex values
+let rainbowColors = [
+  {
+    hex: 'fac89e',
+  },
+  {
+    hex: 'fac89e',
+  }
+];
+
+// creating a 16 x 16 grid of squares
+createGrid(16);
+
+// creating the reset button functionality
+$('#reset').click(() => {
+  let userInput;
+
+  do {
+    userInput = input();
+    if (!Number.isInteger(userInput) || userInput < 1 || userInput > 100) {
+      alert('Wrong value was entered!');
+    }
+  } while (!Number.isInteger(userInput) || userInput < 1 || userInput > 100);
+
+  $('main > div').remove();
+  createGrid(userInput);
+  document.getElementById("gnumber").innerHTML=userInput;
+});
